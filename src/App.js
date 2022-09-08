5f25fd4c00f42f202dcbb389000fd165ea302575
Maksim <grigoriev.ma2014@yandex.ru>
2022-09-08 17:04:54 +0300

    added useReducer

--- src/App.js
+++ src/App.js
@@ -1,29 +1,216 @@
+import React from 'react'
+
 import { Paper, Divider, Button, List, Tabs, Tab } from '@mui/material';
 import { AddField } from './components/AddField';
 import { Item } from './components/Item';
 
+
+const reducer = (state, action) => {
+
+  if (action.type === 'ADD_TASK') {
+    return {
+      ...state,
+      tasks: [
+        ...state.tasks,
+        {
+          id: state.length + 1,
+          text: action.payload.text,
+          completed: action.payload.completed
+        }
+      ]
+    }
+  }
+
+  if (action.type === 'REMOVE_TASK') {
+    return {
+      ...state,
+      tasks: state.tasks.filter(obj => obj.id !== action.payload)
+    }
+  }
+
+  if (action.type === 'ALL_REMOVE') {
+    return {
+      ...state,
+      tasks: []
+    }
+  }
+
+  if (action.type === 'TOGGLE_COMPLETED') {
+    return {
+      ...state,
+      tasks: state.tasks.map(obj => {
+        if (obj.id === action.payload) {
+
+          return {
+            ...obj,
+            completed: !obj.completed
+          }
+
+        }
+        return obj
+      })
+    }
+  }
+
+  if (action.type === 'ALL_COMPLETED') {
+    return {
+      ...state,
+      tasks: state.tasks.map(obj => {
+        return {
+          ...obj,
+          completed: true
+        }
+      })
+    }
+  }
+
+  if (action.type === 'SET_FILTER') {
+    return {
+      ...state,
+      filterBy: action.payload
+    }
+  }
+
+}
+
+
 function App() {
+
+
+  const [state, dispatch] = React.useReducer(reducer,
+
+    {
+      filterBy: 'all',
+      tasks: [
+        {
+          id: 1,
+          text: 'Hello1',
+          completed: false
+        },
+        {
+          id: 2,
+          text: 'World2',
+          completed: true
+        },
+        {
+          id: 3,
+          text: 'World3',
+          completed: false
+        },
+        {
+          id: 4,
+          text: 'World4',
+          completed: true
+        },
+      ]
+    }
+
+  )
+
+
+  const handleClickTask = (text, completed) => {
+    dispatch({
+      type: 'ADD_TASK',
+      payload: {
+        text,
+        completed
+      }
+    })
+  }
+
+
+  const removeTask = (id) => {
+    if (window.confirm(`Вы хотите удалить таску ${id} ?`)) {
+
+      dispatch({
+        type: 'REMOVE_TASK',
+        payload: id
+      })
+
+    }
+  }
+
+  const allRemove = () => {
+    dispatch({
+      type: 'ALL_REMOVE'
+    })
+  }
+
+
+  const toggleCompleted = (id) => {
+    dispatch({
+      type: 'TOGGLE_COMPLETED',
+      payload: id
+    })
+  }
+
+  const allCompleted = () => {
+    dispatch({
+      type: 'ALL_COMPLETED'
+    })
+  }
+
+  const filterIndex = ['all', 'active', 'completed']
+
+  const handleClickTab = (_, i) => {
+    const status = filterIndex[i]
+
+    dispatch({
+      type: 'SET_FILTER',
+      payload: status
+    })
+  }
+
+
   return (
     <div className="App">
       <Paper className="wrapper">
         <Paper className="header" elevation={0}>
           <h4>Список задач</h4>
         </Paper>
-        <AddField />
+        <AddField addTask={handleClickTask} />
         <Divider />
-        <Tabs value={0}>
+        <Tabs onChange={handleClickTab} value={filterIndex.findIndex(i => i === state.filterBy)}>
           <Tab label="Все" />
           <Tab label="Активные" />
           <Tab label="Завершённые" />
         </Tabs>
         <Divider />
         <List>
-          <Item text="Задача №1" />
+
+          {
+            state.tasks.filter(obj => {
+
+              if (state.filterBy === 'all') {
+                return true
+              }
+
+              if (state.filterBy === 'active') {
+                return !obj.completed
+              }
+
+              if (state.filterBy === 'completed') {
+                return obj.completed
+              }
+
+
+
+            }).map(obj => <Item
+              key={obj.id}
+              text={obj.text}
+              completed={obj.completed}
+              removeTask={() => removeTask(obj.id)}
+              toggleCompleted={() => toggleCompleted(obj.id)}
+            />
+            )
+          }
+
+
         </List>
         <Divider />
         <div className="check-buttons">
-          <Button>Отметить всё</Button>
-          <Button>Очистить</Button>
+          <Button onClick={allCompleted}>Отметить всё</Button>
+          <Button onClick={allRemove}>Очистить</Button>
         </div>
       </Paper>
     </div>
--- src/components/AddField.jsx
+++ src/components/AddField.jsx
@@ -1,18 +1,35 @@
+import React from 'react'
+
 import { TextField, Button, Checkbox } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 
-export const AddField = () => {
+export const AddField = ({ addTask }) => {
+
+  const [value, setValue] = React.useState('')
+
+  const [isCompleted, setIsCompleted] = React.useState(false)
+
+
+  const handleClickBtn = () => {
+    addTask(value, isCompleted)
+
+    setValue('')
+    setIsCompleted(false)
+  }
+
   return (
     <div className="field">
       <Checkbox
+        checked={isCompleted}
+        onChange={e => setIsCompleted(e.target.checked)}
         className="checkbox"
         icon={<RadioButtonUncheckedIcon />}
         checkedIcon={<CheckCircleIcon />}
       />
-      <TextField placeholder="Введите текст задачи..." variant="standard" fullWidth />
-      <Button>
+      <TextField value={value} onChange={e => setValue(e.target.value)} placeholder="Введите текст задачи..." variant="standard" fullWidth />
+      <Button onClick={handleClickBtn}>
         <AddIcon />
       </Button>
     </div>
